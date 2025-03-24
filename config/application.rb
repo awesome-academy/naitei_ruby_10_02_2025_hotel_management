0db2d649676a426e128bdf7349585df01fd63436
lam-vqh0466 <vu.quang.hoang.lam@sun-asterisk.com>
2025-03-24 11:10:07 +0700

    init project1

---
+++ config/application.rb
@@ -0,0 +1,14 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsTutorial
+  class Application < Rails::Application
+    config.load_defaults 7.0
+    config.i18n.available_locales = %i[en vi]
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+<<<<<<< HEAD
+=======
+   root "pages#home"
+   get "static_pages/home"
+>>>>>>> 17855a6 (init project1)
+end
