ccf3e6058579dfa076139f829f33a9a8eaf21f96
tungnx-0469 <nguyen.xuan.tung-d@sun-asterisk.com>
2025-04-07 23:00:04 +0700

    view room calendar

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,23 @@
 class Room < ApplicationRecord
   belongs_to :room_type
   has_many :stay_ats, dependent: :destroy
-  has_many :request, through: :stay_ats
+  has_many :requests, through: :stay_ats
+
+  def booked_days_in_month year, month
+    start_of_month = Date.new(year, month, 1)
+    end_of_month = start_of_month.end_of_month
+
+    booked_days = []
+
+    requests.where(
+      "(checkin_date <= ?) AND (checkout_date >= ?)",
+      end_of_month, start_of_month
+    ).find_each do |request|
+      (request.checkin_date...request.checkout_date).each do |date|
+        booked_days << date.day if date.month == month && date.year == year
+      end
+    end
+
+    booked_days.uniq.sort
+  end
 end
