2943a309d31dcd3568dc8d112b06c8c94fc59ff5
tungnx-0469 <nguyen.xuan.tung-d@sun-asterisk.com>
2025-04-08 14:49:36 +0700

    create room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_07_141017) do
+ActiveRecord::Schema[7.0].define(version: 2025_04_08_074804) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -126,6 +126,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_07_141017) do
     t.integer "floor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["room_number"], name: "index_rooms_on_room_number", unique: true
     t.index ["room_type_id"], name: "index_rooms_on_room_type_id"
   end
 
